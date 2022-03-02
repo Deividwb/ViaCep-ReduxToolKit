cb49a856190653a697b607dcc940a7fc3fddabc2
Deividwb <bernardinodeivid@gmail.com>
2022-03-02 11:01:07 -0300

    Add Modal

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,16 @@
+import './styles.css';
+
 const NavBar = () => {
-    return (
-        <h1>Navbar</h1>
-    );
-}
+  return (
+    <header class="nav-container">
+      <nav class="nav-main">
+        <h1>Via CEP</h1>
+        <div class="contact-link">Buscar Cep</div>        
+      </nav>
+    
+    </header>    
+    
+  );
+};
 
 export default NavBar;
--- src/pages/ListCep/index.js
+++ src/pages/ListCep/index.js
@@ -15,22 +15,113 @@ const ListCep = () => {
       siafi: "7107",
       numero: "50",
     },
+    {
+      cep: "01001-999",
+      logradouro: "Rua do Zé",
+      complemento: "lado ímpar",
+      bairro: "Sé",
+      localidade: "Minas",
+      uf: "SP",
+      ibge: "3550308",
+      gia: "1004",
+      ddd: "33",
+      siafi: "7107",
+      numero: "88",
+    },
   ];
 
   return (
-    <div className="card">
+    <>
       {cepMock.map((cep) => (
         <>
-          <h5 className="card-header card-city">{cep.localidade}</h5>
-          <div className="card-body">
-            <h5 className="card-title">{`${cep.logradouro}, ${cep.numero}`}</h5>
-            <p className="card-text">
-              {cep.cep}
-            </p>
-          </div>
+          <section id="list-section" class="list-container">
+            <div class="card-list-item">
+              <h3>São Paulo</h3>
+              <p class="address-line">Rua dos jardins,720</p>
+              <p class="address-cep">11200-345</p>
+            </div>
+          </section>
         </>
       ))}
-    </div>
+
+
+      {/* modal */}
+
+      <section id="modal-contact" class="modal-container ">
+        <div class="modal-box">
+          <div class="modal-content">
+            {/* formulario   */}
+
+            <section id="form-section" class="form-container">
+              <div class="card-form">
+                <form name="newAddress">
+                  <div class="form-control">
+                    <label for="cep">CEP:</label>
+                    <input
+                      name="cep"
+                      id="cep"
+                      class="form-input"
+                      type="text"
+                      placeholder="CEP"
+                    />
+                    <div class="error-message" data-error="cep"></div>
+                  </div>
+                  <div class="form-control">
+                    <label for="street">Logradouro:</label>
+                    <input
+                      name="street"
+                      id="street"
+                      class="form-input"
+                      type="text"
+                      placeholder="Logradouro"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-control">
+                    <label for="number">Número:</label>
+                    <input
+                      name="number"
+                      id="number"
+                      class="form-input"
+                      type="text"
+                      placeholder="Número"
+                    />
+                    <div class="error-message" data-error="number"></div>
+                  </div>
+                  <div class="form-control">
+                    <label for="city">Cidade:</label>
+                    <input
+                      name="city"
+                      id="city"
+                      class="form-input"
+                      type="text"
+                      placeholder="Cidade"
+                      disabled
+                    />
+                  </div>
+                  <div class="btn-container">
+                    <button name="btnSave" class="btn btn-left" type="submit">
+                      Salvar
+                    </button>
+                    <button name="btnClear" class="btn btn-right">
+                      Limpar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </section>
+
+            {/* ///////// */}
+          </div>
+          <button id="modal-contact-close" class="btn">
+            Fechar
+          </button>
+        </div>
+
+      </section>
+
+      
+    </>
   );
 };
 
